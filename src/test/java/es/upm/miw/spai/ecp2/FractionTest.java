b1590711d229844bbc94f00e5907881eeebd7191
David Blasco <dblasco@gmail.com>
2016-09-26 21:10:06 +0200

    Genero TESTS Fraction y User.

---
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -0,0 +1,39 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FractionTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Test
+    public void testFractionIntInt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFraction() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNumerator() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetDenominator() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDecimal() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -0,0 +1,44 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testUser() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNumber() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetFamilyName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFullName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testInitials() {
+        fail("Not yet implemented");
+    }
+
+}
